3f0123d31903fe33349947043f5d879a1571072b
Adrian Helberg <bostelmann@hanseaticsoft.com>
2021-07-13 15:07:41 +0000

    VueJS Curs 05

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # TFT-Wizard
 
 ## A fresh envorinment can be found under branch fresh-env
+
+## Lernkurve
+# v-show vs. v-if
+<div v-show="exp">...</div> versicht das dom-element lediglich mit einem display:none, wenn exp falsy ist
+<div v-if="exp">...</div> hängt das element nur an den dom, wenn exp truly ist
+
+# v-for
+<div v-for="element in elements" v-bind:key="element.id">{{element.name}}</div> braucht einen eindeutigen key, um saubere Updates zu ermöglichen
+# Iteration
+<div v-for="n in 10" v-bind:key="n">{{n}}</div>
+# Attribute dynamisch binden
+<a :href="`?id=${index}`">...</a> ist die Kurzschreibweise für <a v-bind:href="`?id=${index}`">...</a> also v-bind kann weggelassen werden
+# Methoden
+<button @click="fun"></button> ist die Kurzform von <button v-on:click="test">Test</button> Methoden können dynamisch über ein '@' gebinded werden
--- tft-wizard/src/App.vue
+++ tft-wizard/src/App.vue
@@ -1,23 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    {{ greetings }}
+    <div class="age">
+      <span v-if="alter < 18">minderjährig</span>
+      <span v-else-if="alter >= 18">volljährig</span>
+    </div>
+
+    <h3>Wohnorte:</h3>
+    <ul>
+      <li v-for="city in cities" v-bind:key="city.id">
+        {{ city.id }}. {{ city.name }}
+      </li>
+    </ul>
+
+    <span v-for="n in 10" v-bind:key="n">{{ n }}</span>
+
+    <ul>
+      <li v-for="(todo, index) in todos" v-bind:key="todo.id" :class="{'finished': todo.finished}">
+        <input type="checkbox" :checked="todo.finished" />
+        <a :href="`?id=${index}`">Show</a>
+        {{ ++index }}. {{ todo.name }}
+      </li>
+    </ul>
+
+    <button @click="test">Test</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
-
-@Component({
-  components: {
-    HelloWorld,
+export default {
+  name: "App",
+  data: () => ({
+    vorname: "Adrian",
+    nachname: "Helberg",
+    alter: 29,
+    cities: [
+      { id: 1, name: "Stuttgart" },
+      { id: 2, name: "Hamburg" },
+      { id: 3, name: "Berlin" },
+      { id: 4, name: "Köln" },
+    ],
+    todos: [
+      { id: 0, name: "Todo #1", finished: false },
+      { id: 1, name: "Todo #2", finished: true },
+      { id: 2, name: "Todo #3", finished: false },
+      { id: 3, name: "Todo #4", finished: false },
+    ],
+  }),
+  methods: {
+    test() { 
+      alert("Hi!"); 
+    }
   },
-})
-export default class App extends Vue {}
+  computed: {
+    greetings() {
+      return `Hi, ${this.vorname} ${this.nachname}!`;
+    },
+  },
+};
 </script>
 
-<style lang="sass">
+<style>
+.finished {
+  text-decoration: line-through;
+}
+.age {
+  border: 1px solid red;
+  margin: 5px;
+  padding: 10px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
